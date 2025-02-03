ad11b29e63cc8c0a0c2160e0ab6fe5e63d9164e8
garylee920209 <7369zy@gmail.com>
2025-02-03 22:14:05 +0800

    update sh

--- new_ws_finetune.sh
+++ new_ws_finetune.sh
@@ -1,14 +1,14 @@
 set -e
 exp_name="fine_tune_trainable_with_new_token_corrected"
 size=large-v2
-dir=data
+dir=/Users/garylee/Desktop/project/MLSUPERB2-Challenge
 output_dir=outputs/$exp_name/all
-python3 ws_finetune_trainable.py \
+python3 new_ws_finetune.py \
     --size $size \
     --batch 1 \
     --grad_accum 8 \
     --epoch 5 \
-    --custom_set_train $dir/train.csv \
-    --custom_set_test $dir/test_val.csv \
+    --custom_set_train $dir/train_10min.csv \
+    --custom_set_test $dir/test_30.csv \
     --all \
     --output_dir $output_dir > $output_dir/output.log
